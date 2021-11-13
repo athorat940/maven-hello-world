9319977cdeb8575c84dcb2b264f78a3221b3a194
athorat940 <92424910+athorat940@users.noreply.github.com>
2021-11-13 18:35:03 +0530

    Update AppTest.java

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,8 +1,10 @@
 package com.mycompany.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.openga.selenium.WebDriver;
+import org.openga.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testing.Assert;
+import org.testing.annotations.Test;
 
 /**
  * Unit test for simple App.
@@ -33,6 +35,31 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+       WebDriverManager.chromedriver().browserVersion("94.0.4606.81").setup();  
+       WebDriver driver = new ChromeDriver();
+    	
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(baseUrl);
+
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+       
+        //close Fire fox
+        driver.close();
+       // assertTrue( true );
     }
 }
